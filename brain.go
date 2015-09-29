705b019d011445363043025c1f2b89d27082444c
mukul rawat <rawatmukul86@gmail.com>
2015-09-30 03:30:26 +0530

    Set up our data structure and created basic methods for it

---
+++ brain.go
@@ -0,0 +1,49 @@
+// This is a simple implementation of a Brainfuck interpreter in Golang
+
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "log"
+    "os"
+)
+
+// Here we define our tape data structure
+// Tape is a slice of 8-bit unsigned integers. 
+// We will be getting ASCII values from the user as input, uint8 
+// are perfect to store them.
+// We also have a pointer that points to the current cell in the tape.
+// We can increment or decrement this pointer.
+type data struct{
+    tape []uint8
+    ptr int
+}
+
+// Now here we will define some methods on our data strucutre.
+
+// Initialize our tape data structure
+func (*d data) initialize(){
+    d.tape = make([]uint8, 30000)
+    ptr = 0
+}
+
+// Increment the pointer
+func (*d data) increment_ptr(){
+    d.ptr += 1
+}
+
+// Decrement the pointer
+func (*d data) decrement_ptr(){
+    d.ptr -= 1
+}
+
+// Increment the value in current cell
+func (*d data) increment_value(){
+    d.tape[d.ptr] += 1
+}
+
+// Decrement the value in current cell
+func (*d data) decrement_value(){
+    d.tape[d.ptr] -= 1
+}
